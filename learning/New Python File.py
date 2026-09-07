7adeadf0e21e68304ab68dd8d8563107b9d65d8f
RuchikaNikam <ruchikanikam39@gmail.com>
2026-09-07 22:23:24 +0530

    Create New Python File.py

---
+++ learning/New Python File.py
@@ -0,0 +1,13 @@
+# EcoSmart AI - Day 1
+
+project_name = "EcoSmart AI"
+
+print("Welcome to", project_name)
+print("AI-Powered Smart Building Energy Management System")
+
+print("\nOur future modules:")
+print("1. Energy Analytics")
+print("2. Machine Learning Prediction")
+print("3. Anomaly Detection")
+print("4. RAG AI Assistant")
+print("5. Smart Dashboard")
